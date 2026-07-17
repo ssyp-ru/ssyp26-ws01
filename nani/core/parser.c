1eb914ae6c8045277c6b0c5bc1e1459d8eda17e6
myxo <klimov.ns@mail.ru>
2026-07-17 08:09:39 +0700

    fix compilation errors

--- nani/core/parser.c
+++ nani/core/parser.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include "parser.h"
 #include "lexer.h"
-#include "value.h"
 
 [[noreturn]] static void err(tokens_t* tokens, int* pos, const char* text) {
 	token_t* tok = &tokens->ptr[*pos];
@@ -11,22 +10,24 @@
 	exit(1);
 }
 
-static stmt_t* add_stmt(program_t* ast) {
-	if (!ast->ptr) {
-		ast->length = 0;
+static stmt_t* add_stmt(stmt_list_t* ast) {
+	if (!ast->items) {
+		ast->count = 0;
 		ast->capacity = 16;
-		ast->ptr = malloc(ast->capacity * sizeof(stmt_t));
+		ast->items = malloc(ast->capacity * sizeof(stmt_t*));
 	}
 
-	if (ast->length == ast->capacity) {
+	if (ast->count == ast->capacity) {
 		ast->capacity *= 2;
-		ast->ptr = realloc(ast->ptr, ast->capacity * sizeof(stmt_t));
+		ast->items = realloc(ast->items, ast->capacity * sizeof(stmt_t*));
 	}
 
-	return &ast->ptr[ast->length++];
+    stmt_t* stmt = malloc(sizeof(stmt_t));
+	ast->items[ast->count++] = stmt;
+    return stmt;
 }
 
-void free_ast(program_t* ast) {
+void free_ast(stmt_list_t* ast) {
 	// TODO
 }
 
@@ -40,9 +41,10 @@ static expr_t* parse_factor(tokens_t* tokens, int* pos);
 static expr_t* parse_unary(tokens_t* tokens, int* pos);
 static expr_t* parse_primary(tokens_t* tokens, int* pos);
 
-void parse_program(program_t* ast, tokens_t* tokens) {
+void parse_program(stmt_list_t* ast, tokens_t* tokens) {
 	int pos = 0;
 
+    // TODO: continue parsing statements until eof
 	stmt_t* stmt = add_stmt(ast);
 	parse_stmt(stmt, tokens, &pos);
 }
@@ -51,31 +53,32 @@ void parse_stmt(stmt_t* stmt, tokens_t* tokens, int* pos) {
 	token_t* tok = &tokens->ptr[*pos];
 	(*pos)++;
 
-	if (tok->kind == TOKEN_ASSERT) stmt->kind = STMT_ASSERT;
-	else if (tok->kind == TOKEN_PRINT) stmt->kind = STMT_PRINT;
+	if (tok->type == TOKEN_ASSERT) stmt->type = STMT_ASSERT;
+	else if (tok->type == TOKEN_PRINT) stmt->type = STMT_PRINT;
 	else err(tokens, pos, "Expected statement (assert or print)");
-	
-	stmt->expr = parse_or(tokens, pos);
+
+    // hack
+	stmt->as.print.expression = parse_or(tokens, pos);
 
 	tok = &tokens->ptr[*pos];
 	(*pos)++;
-	if (tok->kind != TOKEN_SEMICOLON) err(tokens, pos, "Expected semicolon");
+	if (tok->type != TOKEN_SEMICOLON) err(tokens, pos, "Expected semicolon");
 }
 
 expr_t* parse_or(tokens_t* tokens, int* pos) {
 	expr_t* left = parse_and(tokens, pos);
 
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_OR) return left;
+	if (tok->type != TOKEN_OR) return left;
 	(*pos)++;
 
 	expr_t* right = parse_and(tokens, pos);
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_BINARY;
-	out->binary.op = TOKEN_OR;
-	out->binary.l = left;
-	out->binary.r = right;
+	out->type = EXPR_LOGICAL;
+	out->value.logical.op = *tok;
+	out->value.logical.left = left;
+	out->value.logical.right = right;
 	return out;
 }
 
@@ -83,16 +86,16 @@ expr_t* parse_and(tokens_t* tokens, int* pos) {
 	expr_t* left = parse_equality(tokens, pos);
 
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_AND) return left;
+	if (tok->type != TOKEN_AND) return left;
 	(*pos)++;
 
 	expr_t* right = parse_equality(tokens, pos);
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_BINARY;
-	out->binary.op = TOKEN_AND;
-	out->binary.l = left;
-	out->binary.r = right;
+	out->type = EXPR_LOGICAL;
+	out->value.logical.op = *tok;
+	out->value.logical.left = left;
+	out->value.logical.right = right;
 	return out;
 }
 
@@ -100,16 +103,16 @@ expr_t* parse_equality(tokens_t* tokens, int* pos) {
 	expr_t* left = parse_comparison(tokens, pos);
 
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_EQ_EQ && tok->kind != TOKEN_BANG_EQ) return left;
+	if (tok->type != TOKEN_EQ && tok->type != TOKEN_BANG_EQ) return left;
 	(*pos)++;
 
 	expr_t* right = parse_comparison(tokens, pos);
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_BINARY;
-	out->binary.op = tok->kind;
-	out->binary.l = left;
-	out->binary.r = right;
+	out->type = EXPR_LOGICAL;
+	out->value.logical.op = *tok;
+	out->value.logical.left = left;
+	out->value.logical.right = right;
 	return out;
 }
 
@@ -117,16 +120,16 @@ expr_t* parse_comparison(tokens_t* tokens, int* pos) {
 	expr_t* left = parse_term(tokens, pos);
 
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_LESS && tok->kind != TOKEN_LESS_EQ && tok->kind != TOKEN_GREATER && tok->kind != TOKEN_GREATER_EQ) return left;
+	if (tok->type != TOKEN_LESS && tok->type != TOKEN_LESS_EQ && tok->type != TOKEN_GREATER && tok->type != TOKEN_GREATER_EQ) return left;
 	(*pos)++;
 
 	expr_t* right = parse_term(tokens, pos);
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_BINARY;
-	out->binary.op = tok->kind;
-	out->binary.l = left;
-	out->binary.r = right;
+	out->type = EXPR_LOGICAL;
+	out->value.logical.op = *tok;
+	out->value.logical.left = left;
+	out->value.logical.right = right;
 	return out;
 }
 
@@ -134,16 +137,16 @@ expr_t* parse_term(tokens_t* tokens, int* pos) {
 	expr_t* left = parse_factor(tokens, pos);
 
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_PLUS && tok->kind != TOKEN_MINUS) return left;
+	if (tok->type != TOKEN_PLUS && tok->type != TOKEN_MINUS) return left;
 	(*pos)++;
 
 	expr_t* right = parse_factor(tokens, pos);
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_BINARY;
-	out->binary.op = tok->kind;
-	out->binary.l = left;
-	out->binary.r = right;
+	out->type = EXPR_BINARY;
+	out->value.binary.op = *tok;
+	out->value.binary.left = left;
+	out->value.binary.right = right;
 	return out;
 }
 
@@ -151,28 +154,28 @@ expr_t* parse_factor(tokens_t* tokens, int* pos) {
 	expr_t* left = parse_unary(tokens, pos);
 
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_STAR && tok->kind != TOKEN_SLASH) return left;
+	if (tok->type != TOKEN_STAR && tok->type != TOKEN_SLASH) return left;
 	(*pos)++;
 
 	expr_t* right = parse_unary(tokens, pos);
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_BINARY;
-	out->binary.op = tok->kind;
-	out->binary.l = left;
-	out->binary.r = right;
+	out->type = EXPR_BINARY;
+	out->value.binary.op = *tok;
+	out->value.binary.left = left;
+	out->value.binary.right = right;
 	return out;
 }
 
 expr_t* parse_unary(tokens_t* tokens, int* pos) {
 	token_t* tok = &tokens->ptr[*pos];
-	if (tok->kind != TOKEN_BANG && tok->kind != TOKEN_MINUS) return parse_primary(tokens, pos);
+	if (tok->type != TOKEN_BANG && tok->type != TOKEN_MINUS) return parse_primary(tokens, pos);
 	(*pos)++;
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_UNARY;
-	out->unary.op = tok->kind;
-	out->unary.operand = parse_unary(tokens, pos);
+	out->type = EXPR_UNARY;
+	out->value.unary.op = *tok;
+	out->value.unary.right = parse_unary(tokens, pos);
 	return out;
 }
 
@@ -180,34 +183,28 @@ expr_t* parse_primary(tokens_t* tokens, int* pos) {
 	token_t* tok = &tokens->ptr[*pos];
 	(*pos)++;
 
-	if (tok->kind == TOKEN_LEFT_PAREN) {
+	if (tok->type == TOKEN_LEFT_PAREN) {
 		expr_t* expr = parse_or(tokens, pos);
 
 		token_t* tok2 = &tokens->ptr[*pos];
 		(*pos)++;
-		if (tok2->kind != TOKEN_RIGHT_PAREN) err(tokens, pos, "Expected right parentheses");
+		if (tok2->type != TOKEN_RIGHT_PAREN) err(tokens, pos, "Expected right parentheses");
 
 		return expr;
 	}
 
 	expr_t* out = malloc(sizeof(expr_t));
-	out->kind = EXPR_LITERAL;
+	out->type = EXPR_LITERAL;
+    out->value.literal.lexeme = *tok;
 
-	if (tok->kind == TOKEN_NUMBER) {
-		char* endptr = (char*) &tokens->code[tok->start + tok->len];
+	if (tok->type == TOKEN_NUMBER) {
+		out->value.literal.type = LITERAL_NUMBER;
+	} else if (tok->type == TOKEN_TRUE || tok->type == TOKEN_FALSE) {
+		out->value.literal.type = LITERAL_BOOL;
+	} else {
+        free(out);
+        err(tokens, pos, "Expected primary (expr in parentheses, number, true or false)");
+    }
 
-		out->literal.val.kind = VAL_NUM;
-		out->literal.val.num = strtod(&tokens->code[tok->start], &endptr);
-		return out;
-	}
-
-	if (tok->kind == TOKEN_TRUE || tok->kind == TOKEN_FALSE) {
-		out->literal.val.kind = VAL_BOOL;
-		out->literal.val.boolean = tok->kind == TOKEN_TRUE;
-		return out;
-	}
-
-	free(out);
-	err(tokens, pos, "Expected primary (expr in parentheses, number, true or false)");
+	return out;
 }
-
